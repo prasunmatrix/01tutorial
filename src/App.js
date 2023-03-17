97ada22afa079ec5e34e1941ee5596b41e3d0139
prasunmatrix <prasun@matrixnmedia.com>
2023-03-17 19:27:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
