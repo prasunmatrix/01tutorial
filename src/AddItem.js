3fd6089665c9394dd9ef92144716608f1887bb43
prasunmatrix <prasun@matrixnmedia.com>
2023-05-05 19:05:49 +0530

    addItem and app js update

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { FaPlus } from 'react-icons/fa';
 
-const AddItem = () => {
+const AddItem = ({newItem,setNewItem,handleSubmit}) => {
   return (
     <form className='addForm'>
       <label htmlFor='addItem'>Add Item</label>
-      <input autoFocus id='addItem' type='text' placeholder='Add Item' required />
-      <button type='submit' aria-label='Add Item'></button>
+      <input autoFocus id='addItem' type='text' placeholder='Add Item' required  value={newItem} onChange={(e)=>setNewItem(e.target.value)}/>
+      <button type='submit' aria-label='Add Item'>
+        <FaPlus/>
+      </button>
+      
     </form>
   )
 }
--- src/App.js
+++ src/App.js
@@ -2,47 +2,60 @@
 //import './App.css';
 import React from 'react';
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
 
 function App() {
-  const [items,setItems]=useState([
+  const [items, setItems] = useState([
     {
-      id:1,
-      checked:true,
-      item:"one half pound of bag of Cocoa Covered Almonds Unsalted"
+      id: 1,
+      checked: true,
+      item: "one half pound of bag of Cocoa Covered Almonds Unsalted"
     },
     {
-      id:2,
-      checked:false,
-      item:"Item 2"
+      id: 2,
+      checked: false,
+      item: "Item 2"
     },
     {
-      id:3,
-      checked:false,
-      item:"Item 3"
+      id: 3,
+      checked: false,
+      item: "Item 3"
     }
   ]);
 
-  const handleCheck=(id)=>{
-    const listItems=items.map((item)=> item.id === id ? {...item,checked:!item.checked}:item);
+  const [newItem, setNewItem] = useState('')
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
-    localStorage.setItem('shopping',JSON.stringify(listItems));
+    localStorage.setItem('shopping', JSON.stringify(listItems));
   }
-  
-  const handleDelete=(id) =>{
+
+  const handleDelete = (id) => {
     //console.log(id);
-    const listItems=items.filter((item)=>item.id!==id);
+    const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
-    localStorage.setItem('shopping',JSON.stringify(listItems));
+    localStorage.setItem('shopping', JSON.stringify(listItems));
   }
+
+  const handleSubmit = (e) => {
+    console.log('submitted')
+  }
+
   return (
     <div className="App">
-      <Header title="Grocery List"/>
-      <Content 
-        items={items}  handleCheck={handleCheck} handleDelete={handleDelete} />
-      <Footer length={items.length}/>
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
+      <Footer length={items.length} />
     </div>
   );
 }
